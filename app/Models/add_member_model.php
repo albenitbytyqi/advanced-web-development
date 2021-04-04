f2e2b771bf85f3eb825761edac48323024241628
Albenit <35097523+Albenit@users.noreply.github.com>
2021-04-04 18:44:25 +0200

    add functions to model for db

--- app/Models/add_member_model.php
+++ app/Models/add_member_model.php
@@ -8,6 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class add_member_model extends Model
 {
     use HasFactory;
+  //  public $timestamps = false;
+    public $table = 'members';
 
 
+    public function getId(){
+        return $this->id;
+    }
+    public function getFirstName(){
+        return $this->first_name;
+    }
+    public function getLastName(){
+        return $this->last_name;
+    }
+    public function getBirthdate(){
+        return $this->birthdate;
+    }
+    public function getExpireDate(){
+        return $this->expire_date;
+    }
+    public function getProfilePicture(){
+        return $this->profile_picture;
+    }
 }
