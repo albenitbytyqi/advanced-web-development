ad9add147230be47d152cd9265606aa6e98494ac
Albenit <35097523+Albenit@users.noreply.github.com>
2021-04-04 17:40:08 +0200

    Create database migration

---
+++ database/migrations/2021_04_04_151751_gym_member.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class GymMember extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birthdate');
+            $table->date('expire_date');
+            $table->string('profile_picture')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
