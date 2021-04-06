4bfa469c6e2cc9e4ed1d14b4d8d45684c7f6ea2e
Albenit <35097523+Albenit@users.noreply.github.com>
2021-04-06 19:21:57 +0200

    create blade for view Member

---
+++ resources/views/viewGymMember.blade.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>View Member</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    </head>
+    <body>
+            <center>
+            <a class="btn btn-success" href="{{route('addmember')}}">Add Member</a>
+            </center>
+            <?php $gymModel = new \App\Models\add_member_model();
+                $members = $gymModel::all();
+            ?>
+            @foreach($members as $member)
+            @component('components.tablerow')
+                @slot('id', $member->getId())
+                @slot('firstName', $member->getFirstName())
+                @slot('lastName', $member->getLastName())
+                @slot('birthdate', $member->getBirthdate())
+                @slot('expireDate', $member->getExpireDate())
+                @slot('delete')
+                    <form onsubmit="return confirm('Are your sure to delete {{$member->getFirstName()}}');"
+                          method="POST" action="{{route('deleteMember',$member->getId())}}">
+                        @method('DELETE')
+                        @csrf
+                        <button class="btn btn-danger">DELETE</button>
+                    </form>
+                @endslot
+            @endcomponent
+            @endforeach
+    </body>
+</html>
