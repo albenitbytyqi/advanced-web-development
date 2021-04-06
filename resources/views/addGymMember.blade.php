2da580814488fd81e039d72bf3f494e14c0e17c5
Albenit <35097523+Albenit@users.noreply.github.com>
2021-04-06 19:21:20 +0200

    add bootstrap at addGymMember form

--- resources/views/addGymMember.blade.php
+++ resources/views/addGymMember.blade.php
@@ -1,11 +1,13 @@
 <html>
 <head>
     <title>Add Gym Member</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 </head>
     <body>
     <center>
         <form method="POST" action="{{route('add_member_gym')}}" enctype="multipart/form-data">
             @csrf
+            <a class="btn btn-primary" href="{{route('viewGymMember')}}"> View Member</a><br><br>
             <label>First Name</label><br>
             <input type="text" name="first_name"><br>
             <label>Last Name</label><br>
@@ -15,8 +17,8 @@
             <label>Expire Date</label><br>
             <input type="date" name="expire_date"><br>
             <label>Profile Picture</label><br>
-            <input type="file" name="profile_picture"><br>
-            <input type="submit" value="Save">
+            <input type="file" name="profile_picture"><br><br>
+            <input class="btn btn-success" type="submit" value="Save">
         </form>
     </center>
     </body>
