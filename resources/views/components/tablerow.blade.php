bc72226f104c5c52dc065a6d13ba2eb6c7d4059b
Albenit <35097523+Albenit@users.noreply.github.com>
2021-04-06 17:22:13 +0200

    create tablerow blade

---
+++ resources/views/components/tablerow.blade.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <table class="table">
+            <thead>
+            <tr>
+                <td>{{$id}} <br>{{$firstName}}<br>{{$lastName}}<br>{{$birthdate}}<br>{{$expireDate}} {{$delete}}</td>
+            </tr>
+            </thead>
+        </table>
+    </div>
+</div>
+
+
+
