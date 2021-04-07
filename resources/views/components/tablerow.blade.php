7c21a7003fb6d2e3552d15acebe642c28eb9e65d
Albenit <35097523+Albenit@users.noreply.github.com>
2021-04-07 10:39:14 +0200

    update design of tablerow blade

--- resources/views/components/tablerow.blade.php
+++ resources/views/components/tablerow.blade.php
@@ -3,7 +3,8 @@
         <table class="table">
             <thead>
             <tr>
-                <td>{{$id}} <br>{{$firstName}}<br>{{$lastName}}<br>{{$birthdate}}<br>{{$expireDate}} {{$edit}} {{$delete}}</td>
+                <td> <b>ID:</b> {{$id}} <br> <b>First Name:</b> {{$firstName}}<br> <b>Last Name:</b>{{$lastName}}<br>
+                    <b>Birthdate</b>: {{$birthdate}}<br> <b>Expire Date:</b> {{$expireDate}} {{$edit}} {{$delete}}</td>
             </tr>
             </thead>
         </table>
