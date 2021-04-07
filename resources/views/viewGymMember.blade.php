2d494f53611ca5fc30657222840f1b3c4277f382
Albenit <35097523+Albenit@users.noreply.github.com>
2021-04-07 10:41:24 +0200

    changes in edit and delete

--- resources/views/viewGymMember.blade.php
+++ resources/views/viewGymMember.blade.php
@@ -4,9 +4,13 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     </head>
     <body>
+
             <center>
+                <br>
             <a class="btn btn-success" href="{{route('addmember')}}">Add Member</a>
+
             </center>
+            <br>
             <?php $gymModel = new \App\Models\add_member_model();
                 $members = $gymModel::all();
             ?>
@@ -18,14 +22,15 @@
                 @slot('birthdate', $member->getBirthdate())
                 @slot('expireDate', $member->getExpireDate())
                 @slot('edit')
-                    <form method="POST" action="{{route('editMember',$member->getId())}}">
+                    <form method="POST" action="{{route('getMember',$member->getId())}}">
                         @method('POST')
                         @csrf
-                        <button class="btn btn-danger">Edit</button>
+                        <br>
+                        <button class="btn btn-info">Edit</button>
                     </form>
                 @endslot
                 @slot('delete')
-                    <form onsubmit="return confirm('Are your sure to delete {{$member->getFirstName()}}');"
+                    <form onsubmit="return confirm('Are you sure to delete {{$member->getFirstName()}}');"
                           method="POST" action="{{route('deleteMember',$member->getId())}}">
                         @method('DELETE')
                         @csrf
